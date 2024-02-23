01eb3a29e705d7d591318df05ed973e320e07d5b
Gbriel2003 <125696008+Gbriel2003@users.noreply.github.com>
2024-02-23 15:54:36 -0400

    Add files via upload

--- suikagame/src/main/java/com/mycompany/suikagame/contenedor.java
+++ suikagame/src/main/java/com/mycompany/suikagame/contenedor.java
@@ -1,9 +1,11 @@
 package com.mycompany.suikagame;
 
-/**
- *
- * @author gabri
- */
+
 public class contenedor {
+    
+    private int dimensiones; //ancho y alto del contenedor
+    private int limite; // numero de frutas que el contenedor puede tener antes de que finalice el juego 
+    private boolean EstadoDeJuego; //Indica si el juego esta en curso o esta terminado si el contenedor se llena
+    private int Movimientos; // cantidad de movimientos realizados por el juegador
     
 }
